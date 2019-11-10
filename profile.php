af5ab6c1a98ecc4e8901f4db465f65eb2a8a3cb4
Amalia Plana <amalia@penni.io>
2019-11-10 14:45:44 +0100

    style

---
+++ profile.php
@@ -0,0 +1,10 @@
+<?php 
+    require_once 'top.php'; 
+?>
+
+<h1>You are now signed in</h1>
+
+<?php 
+    $sLinktoScript = '<script src="js/index.js"></script>';
+    require_once 'bottom.php'; 
+?>
