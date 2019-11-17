8ec6a8f027e1436c43a753f2c541fdcc03479f3d
Marija <mari4w04@stud.kea.dk>
2019-11-17 21:24:54 +0100

    Search works

--- bottom.php
+++ bottom.php
@@ -1,14 +1,16 @@
 
 <footer>
+    <img class="footer-logo" src="images/grey-logo.svg">
     2019 Holiday House. Development Environments exam project.
 </footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-<script src="js/validate.js"></script>
+<!-- <script src="js/validate.js"></script> -->
 <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script> -->
-<!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script> -->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+
 
 <?php echo $sLinktoScript ?? ''; ?>
 
